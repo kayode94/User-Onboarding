87d6fe08005eb1c30713d5c0df0b7312788a4366
kayode <kayode.richards@gmail.com>
2020-10-14 16:16:57 -0400

    User Form done

--- react-user-onboarding/src/App.js
+++ react-user-onboarding/src/App.js
@@ -1,18 +1,68 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import UserForm from './Components/UserForm'
 import User from './Components/User'
 import * as yup from 'yup'
-import schema from ''
+import schema from './Components/Validation'
 import './App.css';
 
+//Setting initial States
 
+const initialFormValues = {
+   first_name:'',
+   last_name: '',
+   email: '',
+   password:'',
+   avatar: '',
+   id:`${Math.floor(Math.random()* 1000)}`,
+   role: '',
+   //checkboxes
+   terms: false,
+}
 
+console.log(initialFormValues)
+
+const initialFormErros = {
+  name: '',
+  email: '',
+  password: '',
+
+}
+
+const initialUsers = []
+const initialDisabled = true;
 
 function App() {
+  //App states
+  const [users, setUsers] = useState(initialUsers)//array of users
+  const [formValues, setFormValues] = useState(initialFormValues)//an object
+  const [formErrors, setFormErrors] = useState(initialFormErros)//an object
+  const [disabled, setDisabled] = useState(initialDisabled)//boolean
+
+  //on change and on submit functions declared here
+
+  const formSubmit = () =>{
+    const newUser ={
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+    }
+  }
+
+  const inputChange = (name, value) => {
+    setFormValues({...formValues,[name]: value, // NOT AN ARRAY
+});
+  };
+  
   return (
     <div className="App">
       <h1>Hello</h1>
-      <UserForm/>
+      <UserForm 
+      values={formValues}
+      disabled={disabled}
+      error={formErrors}
+      onSubmit={formSubmit}
+      change={inputChange}
+      />
       <User/>
     </div>
   );
--- react-user-onboarding/src/Components/UserForm.js
+++ react-user-onboarding/src/Components/UserForm.js
@@ -1,8 +1,96 @@
 import React from 'react'
 
-const UserForm = ()=>{
+const UserForm = (props)=>{
+    const {values, submit, change, disabled, error} = props
+
+    const onSubmit = event =>{
+        event.preventDefaul();
+        submit();
+    }
+
+    const onChange = event =>{
+        const {name, value, type, checked} = event.target;
+        const valueToUse = type === 'checkbox' ? checked : value;
+        change(name, valueToUse)
+    }
     return(
-        <h2>UserForm Component Rendered Here</h2>
+        <form className = "form-container">
+
+            <div className="form-group submit">
+                <h2>Add a new User</h2>
+                <button disabled={disabled}>Submit</button>
+                
+                {/* rendering any validation erros */}
+                <div className='errors'>
+                    <div>{error.name}</div>
+                    <div>{error.password}</div>
+                    <div>{error.email}</div>
+                </div>
+            </div>
+
+            <div className='form-group inputs'>
+                <h4>General Information</h4>
+                <label>
+                    First Name{' '}
+                    <input 
+                    defaultValue={values.first_name}
+                    onChange={onChange}
+                    name='name'
+                    type='text'
+                    />
+                </label>
+
+                <label>
+                    Last Name{' '}
+                    <input 
+                    defaultValue={values.last_name}
+                    onChange={onChange}
+                    name='last-name'
+                    type='text'
+                    />
+                </label>
+
+                <label>
+                    Email{' '}
+                    <input 
+                    defaultValue={values.email}
+                    onChange={onChange}
+                    name='email'
+                    type='email'
+                    />
+                </label>
+                <label>
+                    Password{' '}
+                    <input 
+                    defaultValue={values.password}
+                    onChange={onChange}
+                    name='password'
+                    type='password'
+                    />
+                </label>
+                
+                <label>
+                    Role{' '}
+                    <select onChange={onChange} value={values.role} name='role'>
+                        <option value=''>---Select a Role---</option>
+                        <option value='Student'>Student</option>
+                        <option value='Instructor'>Instructor</option>
+                        <option value='tl'>Team Lead</option>
+                        <option value='Aspirant'>Aspirant</option>
+                    </select>
+                </label>
+                
+                {/* Checkbox Label */}
+                <label>
+                    Terms & Conditions {' '}
+                    <input type='checkbox'
+                    name='terms'
+                    checked={values.terms}
+                    onChange={onChange}
+                    />
+                </label>
+            </div>
+        </form>
     )
 };
 
